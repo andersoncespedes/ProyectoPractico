b509e4ddc7f9b37c8859cdc53c1286713cc13e09
anderson cespedes <andersontrabajos2@gmail.com>
2023-11-08 16:56:46 -0500

    feat(domain): :sparkles: creacion de interfaces

---
+++ Domain/Interface/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interface;
+public interface IGenericRepository<T> where T: BaseEntity
+{
+    void Add(T entity);
+    void AddRange(IEnumerable<T> entities);
+    void Update(T entity);
+    void Remove(T entity);
+    IEnumerable<T> Find(Expression<Func<T,bool>> expression);
+    
+}
